b404cb0198afea801c2544aecee0a40454024e28
cauagreccodev <cauagrecco11@gmail.com>
2026-06-02 23:52:53 -0300

    fix: atualizar dsl do kotlin para jvmTarget 17

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,15 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+allprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
+}
